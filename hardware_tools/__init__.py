3e686ed11352b49255159bfe9dbc95ca6f3ab4d5
Bradley Davis <me@bradleydavis.tech>
2022-08-10 13:46:42 -0700

    Replaced gitsemver with witch-ver

--- hardware_tools/__init__.py
+++ hardware_tools/__init__.py
@@ -3,10 +3,8 @@
 A library for automating hardware development and testing
 """
 
-from hardware_tools import version
-
-__version__ = version.version_full
-
 __all__ = ["strformat", "equipment", "measurement", "math", "signal"]
+
+from hardware_tools.version import __version__
 
 from hardware_tools import strformat
